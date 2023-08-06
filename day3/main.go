f5e92f8e2c3be3bc21ccc082151864dba67ee88a
Arthur Santos <arthurvilas5@gmail.com>
2023-08-06 18:05:59 -0300

    Add day 3 part two

--- day3/main.go
+++ day3/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -14,29 +15,31 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	prioritiesSum := 0
+	partOne(bufio.NewScanner(file))
+	_, err = file.Seek(0, 0)
+	if err != nil {
+		log.Fatalln(bufio.NewScanner(file))
+	}
+	partTwo(bufio.NewScanner(file))
+}
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		rucksack := scanner.Text()
+func partOne(s *bufio.Scanner) {
+	prioritiesSum := 0
+	for s.Scan() {
+		rucksack := s.Text()
 		compartment1 := rucksack[:len(rucksack)/2]
 		compartment2 := rucksack[len(rucksack)/2:]
 
-		chars1 := map[rune]bool{}
-		chars2 := map[rune]bool{}
+		chars := map[rune]bool{}
 
 		for _, c := range compartment1 {
-			chars1[c] = true
-		}
-
-		for _, c := range compartment2 {
-			chars2[c] = true
+			chars[c] = true
 		}
 
 		var common rune
 
-		for c := range chars1 {
-			if chars2[c] {
+		for _, c := range compartment2 {
+			if chars[c] {
 				common = c
 				break
 			}
@@ -49,9 +52,67 @@ func main() {
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
+	if err := s.Err(); err != nil {
 		log.Fatalln(err)
 	}
 
 	fmt.Println("Sum of pritorities:", prioritiesSum)
+}
+
+func partTwo(s *bufio.Scanner) {
+	badges := []rune{}
+	group := make([]string, 0, 3)
+	for s.Scan() {
+		if len(group) < cap(group) {
+			group = append(group, s.Text())
+		}
+
+		if len(group) == cap(group) {
+			badge, err := processGroup(group)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			badges = append(badges, badge)
+			group = group[:0]
+		}
+	}
+
+	if err := s.Err(); err != nil {
+		log.Fatalln(err)
+	}
+
+	badgesSum := 0
+	for _, b := range badges {
+		if unicode.IsUpper(b) {
+			badgesSum += int(b) - 'A' + 27
+		} else {
+			badgesSum += int(b) - 'a' + 1
+		}
+	}
+
+	fmt.Println("Sum of badges:", badgesSum)
+}
+
+func processGroup(g []string) (rune, error) {
+	map1 := map[rune]bool{}
+
+	for _, c := range g[0] {
+		map1[c] = true
+	}
+
+	common := map[rune]bool{}
+
+	for _, c := range g[1] {
+		if map1[c] {
+			common[c] = true
+		}
+	}
+
+	for _, c := range g[2] {
+		if common[c] {
+			return c, nil
+		}
+	}
+
+	return ' ', errors.New("no common rune in group")
 }
